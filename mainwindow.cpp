ac47afe68d201d463afb668efb96c30f5012f56a
Weston Gorczyca <weston.gorczyca@gmail.com>
2026-03-31 07:39:43 -0700

    added sphere intersection checking

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,21 @@
 #include "ray.h"
 #include "vec3.h"
 
+bool hit_sphere(const vec3& center, double radius, const ray& r){
+    vec3 c_to_r = center - r.origin();
+
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0*dot(r.direction(), c_to_r);
+    auto c = dot(c_to_r, c_to_r) - radius*radius;
+
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r){
+    // sphere check
+    if(hit_sphere(point3(1, 0, -2), 0.5, r)) return color(1, 0, 0);
+
     vec3 unit_dir = unit_vector(r.direction());
     auto a = 0.5*(unit_dir.y() + 1.0);
     return (1.0 - a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
