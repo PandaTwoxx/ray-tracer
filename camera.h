a56968ad5a77836aadfd347c06334172257afb0a
Weston Gorczyca <weston.gorczyca@gmail.com>
2026-04-01 14:23:29 -0700

    finished first tutorial

--- camera.h
+++ camera.h
@@ -15,6 +15,13 @@ public:
     int height = 405;
     int sample_count = 10;
     int max_bounces = 10;
+    double vfov = 90;
+    point3 lookfrom = point3(0, 0, 0);
+    point3 lookat = point3(0, 0, -1);
+    vec3 vup = vec3(0, 1, 0);
+
+    double defocus_angle = 0;
+    double focus_dist = 10;
 
     camera(double ar, int w): aspect_ratio(ar), width(w) {}
     camera(){}
@@ -56,6 +63,9 @@ private:
     vec3 pixel_delta_u;
     vec3 pixel_delta_v;
     double pixel_ss;
+    vec3 u, v, w;
+    vec3 defocus_disk_u;
+    vec3 defocus_disk_v;
 
     void initialize(){
         // calculate canvas size
@@ -63,24 +73,35 @@ private:
         height = (height < 1) ? 1 : height;
 
         pixel_ss = 1.0 / sample_count;
+        camera_center = lookfrom;
 
         // calculate viewport size and camera
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto theta = degrees_to_radians(vfov);
+        auto h = std::tan(theta/2);
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(width)/height);
-        camera_center = vec3(0, 0, 0);
+
+        //calculate u v w
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
 
         // calculate vectors for viewport edges
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        vec3 viewport_u = viewport_width * u;
+        vec3 viewport_v = viewport_height * -v;
 
         // calculate delta vectors for pixel to pixel
         pixel_delta_u = viewport_u / width;
         pixel_delta_v = viewport_v / height;
 
         // calculate topleft pixel location
-        auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+        auto viewport_upper_left = camera_center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
         fpixel_loc = viewport_upper_left + 0.5*(pixel_delta_u + pixel_delta_v);
+
+        //calculate camera defocus disk vectors
+        auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle/2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
     color ray_color(const ray& r, int depth, const hittable& world){
@@ -120,7 +141,7 @@ private:
     ray get_ray(int i, int j) const {
         auto offset = sample_square();
         auto pixel_sample = fpixel_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
-        auto ray_origin = camera_center;
+        auto ray_origin = (defocus_angle <= 0) ? camera_center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
         return ray(ray_origin, ray_direction);
     }
@@ -130,6 +151,11 @@ private:
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
+    point3 defocus_disk_sample() const {
+        // Returns a random point in the camera defocus disk.
+        auto p = random_in_unit_disk();
+        return camera_center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+    }
 };
 
 #endif // CAMERA_H
--- material.h
+++ material.h
@@ -50,4 +50,41 @@ private:
     double fuzz;
 };
 
+class dielectric : public material {
+public:
+    dielectric(double refraction_index) : refraction_index(refraction_index), albedo(color(1.0, 1.0, 1.0)){}
+    dielectric(double refraction_index, color albedo) : refraction_index(refraction_index), albedo(albedo){}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        attenuation = albedo;
+        double ri = rec.front_face ? (1.0/refraction_index) : refraction_index;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
+
+        vec3 direction;
+
+        if((ri * sin_theta) > 1.0 || reflectance(cos_theta, ri) > random_double()){
+            direction = reflect(unit_direction, rec.normal);
+        }else{
+            direction = refract(unit_direction, rec.normal, ri);
+        }
+
+        scattered = ray(rec.p, direction);
+        return true;
+    }
+
+private:
+    double refraction_index;
+    color albedo;
+
+    static double reflectance(double cosine, double refraction_index) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1 - cosine),5);
+    }
+};
+
 #endif // MATERIAL_H
